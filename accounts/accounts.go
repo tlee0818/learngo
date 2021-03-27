e3abd97d47a444b09d6fb107431dbedfa19491c9
Terrance Lee <takhol@andrew.cmu.edu>
2021-03-27 20:45:30 +0900

    do i have to commit everytime? fuck

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -7,10 +7,20 @@ type Account struct {
 }
 
 // NewAccount creates account
+// doesnt return a copy, but the actual object itself (the address)
 func NewAccount(owner string) *Account {
 	account := Account{
 		owner:   owner,
 		balance: 0}
 
 	return &account
+}
+
+//deposit x amount to the account
+func (a Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+func (a Account) getBalance() int {
+	return a.balance
 }
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/tlee0818/learngo/accounts"
 )
 
 func main() {
-	account := accounts.NewAccount("taco")
-
-	fmt.Println(account)
+	account := accounts.NewAccount("Taco")
+	account.Deposit(10)
+	fmt.Println(account.getBalance())
 
 }
